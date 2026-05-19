f09d400db62e90ccdb9f1978d1d39b912462a339
MY-akeel <akeelapple7@gmail.com>
2026-05-19 18:30:21 +0530

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from ='info@yourwebsite.com';
+$email_from ='info@my-akeel.github.io/university-website';
 
 $email_subject = 'New Form Submission';
 
